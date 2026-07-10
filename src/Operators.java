0353cf2e0bbb12dd0f3dd784c8e54a19a7f24cb5
Rahul Chavan <rc864025@gmail.com>
2026-07-10 11:49:37 +0530

    exercise

---
+++ src/Operators.java
@@ -0,0 +1,23 @@
+public class Operators {
+    public static void main (String[]args){
+
+       // Arithmetic operators
+        int a = 4;
+//        int b = 5*a;
+//        int b = 5%a; module operator
+
+       // Assignment operators
+        int b = 9;
+        b += 3;
+        System.out.println(b);
+        System.out.println(6==8);
+//        System.out.println(5>8); comparison operator
+
+       // logical operators
+        System.out.println(5>3 && 5>2);
+        System.out.println(64>8 || 64>98);
+
+
+    }
+
+}
